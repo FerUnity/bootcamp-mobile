595ec63b56e3ba19c4d02731298fcb2e8a12401d
FerUnity <fdosx2003@gmail.com>
2025-09-22 00:06:45 -0300

    MiCatalogo Initial Commit V2

--- app/src/main/java/com/example/micatalogo/MainActivity.kt
+++ app/src/main/java/com/example/micatalogo/MainActivity.kt
@@ -13,6 +13,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.micatalogo.ui.theme.MiCatalogoTheme
 
+//PROYECTO PARA USAR CON FIREBASE
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
