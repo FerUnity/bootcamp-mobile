f6cb0b5936e71cb03ec0d1cba3da9d68f340ef22
FerUnity <fdosx2003@gmail.com>
2025-11-16 00:51:52 -0300

    Initial Commit

---
+++ MiCalendario/composeApp/src/commonMain/kotlin/com/example/micalendario/ktor/HttpClientFactory.kt
@@ -0,0 +1,14 @@
+package com.example.micalendario.ktor
+
+import io.ktor.client.HttpClient
+import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
+import io.ktor.serialization.kotlinx.json.json
+import kotlinx.serialization.json.Json
+
+object HttpClientFactory {
+    val client = HttpClient {
+        install(ContentNegotiation) {
+            json(Json { ignoreUnknownKeys = true })
+        }
+    }
+}
